719484d5be3cdd07c47f43ec2f69308070a996c4
harshitdeuex <harshit.shah@deuexsolutions.com>
2021-01-06 17:42:27 +0530

    Added all functionalities except compete with friend

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,94 +1,192 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Card from './Card'
+import {gameStart, handleGiveUp, handleHint} from './Card';
 
-let array = [1,2,3,4,5,6];
+let array = [];
+let arrayObject = [];
+let currentLevel = 6;
+let currentStar = 0;
+let level1star = 0, level2star = 0,level3star = 0,level4star = 0,level5star = 0; 
+let totalStars = 0;
+
+const createArray = () => {
+    array = [];
+    for(let i=1; i<=currentLevel; i++){
+        array.push(i);
+    }
+}
+
+const createArrayObject = () => {
+    arrayObject = [];
+    for(let i=1; i<=currentLevel; i++){
+        arrayObject.push({id: i, display: "none", value: array[i-1]});
+    }
+}
 const shuffle = (array) => {
         return array.sort(() => Math.random() - 0.5);
       }
 
-shuffle(array);
+const createBoard = () => {
+    createArray();
+    shuffle(array);
+    createArrayObject();
+}
 
-let arrayObject = [
-    {
-        id: 1,
-        display: "none",
-        value: array[0]
-    },
-    {
-        id: 2,
-        display: "none",
-        value: array[1]
-    },
-    {
-        id: 3,
-        display: "none",
-        value: array[2]
-    },
-    {
-        id: 4,
-        display: "none",
-        value: array[3]
-    },
-    {
-        id: 5,
-        display: "none",
-        value: array[4]
-    },
-    {
-        id: 6,
-        display: "none",
-        value: array[5]
-    },
-]
+const displayCurrentLevelStar = (currentLevel) => {
+    switch(currentLevel){
+        case 6: return level1star;
+                 
+        case 8: return level2star;
+                
+        case 10: return level3star;
+                
+        case 12: return level4star;
+                
+        case 14: return level5star;
+                
+        default: alert("Some error in setting stars");
+    }
+}
 
-const Board = (props) => {
-    const [currentStar, setCurrentStar] = useState(0);
-    const [totalStar, setTotalStar] = useState(0);
+const displayCurrentLevel = (currentLevel) => {
+    switch(currentLevel){
+        case 6: return 1;
+                 
+        case 8: return 2;
+                
+        case 10: return 3;
+                
+        case 12: return 4;
+                
+        case 14: return 5;
+                
+        default: alert("Some error in getting level");
+    }
+}
+createBoard();
+
+const Board = () => {
     const [moves, setMoves] = useState(0);
     const [cardArray, setCardArray] = useState(arrayObject);
 
-    const incrementCurrentStars = () => {
-        if(moves <= 4){
-            setCurrentStar(3);
-        } else if(moves > 4 && moves <= 6){
-            setCurrentStar(2);
+    const setStar = (currentLevel) => {
+        if(moves <= Math.floor(0.75*currentLevel)){
+            currentStar = 3;
+        } else if(moves > Math.floor(0.75*currentLevel) && moves <= currentLevel){
+            currentStar = 2;
         } else {
-            setCurrentStar(1);
+            currentStar = 1;
         };
-        console.log(totalStar);
+        switch(currentLevel){
+            case 6: level1star = currentStar;
+                    break; 
+            case 8: level2star = currentStar;
+                    break;
+            case 10: level3star = currentStar;
+                    break;
+            case 12: level4star = currentStar;
+                    break;
+            case 14: level5star = currentStar;
+                    break;
+            default: alert("Some error in setting stars");
+        }
+        totalStars = level1star + level2star + level3star + level4star + level5star;
     }
-    
     const incrementMoves = () => {
         setMoves(moves + 1);
     }
 
-    const handleRestartLevel = () => {
-        alert("restart");
-        setCurrentStar(0);
+    const resetMoves = () => {
+        setMoves(0);
+    }
+
+    const handleRestartLevel = (currentLevel) => {
+        alert("restarting level");
         setMoves(0);
         setCardArray(arrayObject);
-        window.location.reload();
+        createBoard();
+        gameStart(currentLevel);
+        switch(currentLevel){
+            case 6: level1star = 0;
+                    break; 
+            case 8: level2star = 0;
+                    break;
+            case 10: level3star = 0;
+                    break;
+            case 12: level4star = 0;
+                    break;
+            case 14: level5star = 0;
+                    break;
+            default: alert("Some error in setting stars");
+        }
+    }
+
+    const handleRestartGame = () => {
+        alert("restarting game");
+        currentStar = 0;
+        totalStars = 0;
+        level1star = 0;
+        level2star = 0;
+        level3star = 0;
+        level4star = 0;
+        level5star = 0; 
+        setMoves(0);
+        setCardArray(arrayObject);
+        createBoard();
+        gameStart(currentLevel);
+    }
+
+    const handleLevelClicked = (level) => {
+        setMoves(0);
+        setCardArray(arrayObject);
+        createBoard();
+        gameStart(currentLevel);
+        currentLevel = level;
+        createBoard();
+        setCardArray(arrayObject);
     }
 
     return (
         <div className="wrapper">
             <h1>Memory Game</h1>
+            <div><h3 style={{textAlign: "center"}}>Current Level: {displayCurrentLevel(currentLevel)}</h3></div>
+            <div><h4 style={{textAlign: "center"}}>(You can only use hint once)</h4></div>
+            <div className="levels-container">
+                <ul>
+                    <li onClick={() => handleLevelClicked(6)}>Level 1</li>
+                    <li onClick={() => handleLevelClicked(8)}>Level 2</li>
+                    <li onClick={() => handleLevelClicked(10)}>Level 3</li>
+                    <li onClick={() => handleLevelClicked(12)}>Level 4</li>
+                    <li onClick={() => handleLevelClicked(14)}>Level 5</li>
+                </ul>
+            </div>
+            <div className="all-buttons">
+                <button onClick={() => handleRestartLevel(currentLevel)}>Restart Level</button>
+                <button onClick={() => handleRestartGame()}>Restart Game</button>
+                <button onClick={() => handleGiveUp(currentLevel)}>I Give Up</button>
+                <button id="hint" onClick={() => handleHint(currentLevel)}>Hint</button>
+            </div>
+            
+            <div className="score-card">
+                <p>Moves: {moves}</p>
+                <p>Stars: {displayCurrentLevelStar(currentLevel)}</p>
+                <p>Total Stars: {totalStars}</p>
+            </div>
+            
             <div className="cards-container">
                 {cardArray.map((item) => {
-                    console.log(item);
                     return <Card 
                             key={item.id} 
                             value={Math.ceil(item.value/2)} 
                             id={item.id} 
-                            incrementCurrentStars={incrementCurrentStars}
                             incrementMoves={incrementMoves}
+                            restartLevel={handleRestartLevel}
+                            resetMoves={resetMoves}
+                            currentLevel={currentLevel}
+                            setStar={setStar}
                             />
                 })}
                 </div>
-                <p>Stars: {currentStar}</p>
-                <p>Total Stars: {totalStar}</p>
-                <p>Moves: {moves}</p>
-                <button onClick={handleRestartLevel}>Restart Level</button>
         </div>
     )
 }
--- src/component/Card.js
+++ src/component/Card.js
@@ -5,14 +5,81 @@ let selectedCard2 = 0;
 let value1 = 0;
 let value2 = 0;
 let matchedPairs = 0;
+let cardWidth = "245px";
+let cardHeight = "165px";
+const cardBackgroundColor = "#448AFF";
 
+export const gameStart = (currentLevel) => {
+    selectedCard1 = 0;
+    selectedCard2 = 0;
+    value1 = 0;
+    value2 = 0;
+    matchedPairs = 0;
+    for(let i=1; i<=currentLevel; i++){
+        document.getElementById(i).style.backgroundColor = cardBackgroundColor;
+        document.getElementById(i).style.color = cardBackgroundColor;
+        document.getElementById(i).style.pointerEvents = "auto";
+    }
+    document.getElementById("hint").style.pointerEvents = "auto";
+}
+
+export const handleGiveUp = (currentLevel) => {
+    for(let i=1; i<=currentLevel; i++){
+        document.getElementById(i).style.backgroundColor = "#ffffff";
+        document.getElementById(i).style.color = "rgb(0, 0, 0)";
+        document.getElementById(i).style.pointerEvents = "none";
+    }
+    document.getElementById("hint").style.pointerEvents = "auto";
+}
+
+export const handleHint = (currentLevel) => {
+    let randomNumber = Math.ceil(Math.random()*currentLevel);
+    console.log("random number: " + randomNumber);
+    let color = document.getElementById(randomNumber).style.backgroundColor;
+    console.log("color: " + color)
+    if( color === "rgb(68, 138, 255)"){
+        document.getElementById(randomNumber).style.backgroundColor = "#ffffff";
+        document.getElementById(randomNumber).style.color = "rgb(0, 0, 0)"
+        setTimeout(() => {
+            document.getElementById(randomNumber).style.backgroundColor = cardBackgroundColor;
+            document.getElementById(randomNumber).style.color = cardBackgroundColor;
+        }, 500);
+        document.getElementById("hint").style.pointerEvents = "none";
+    }
+    else {
+        handleHint(currentLevel);
+    }
+}
+
+const cardDimension = (currentLevel) => {
+    if(currentLevel === 8 || currentLevel === 12){
+        cardWidth = "280px";
+        cardHeight = "190px";
+    } else if(currentLevel === 6) {
+        cardWidth = "330px";
+        cardHeight = "290px";
+    } else {
+        cardWidth = "245px";
+        cardHeight = "165px";
+    }
+}
 const Card = (props) => { 
-
+    let currentLevel = props.currentLevel;
+    cardDimension(currentLevel);
+   
+   
     const gameCompleted = () => {
-        if(matchedPairs === 3){
-            matchedPairs = 0;
-            props.incrementCurrentStars();
+        if(matchedPairs === currentLevel/2){
+            props.setStar(currentLevel);
             alert("congratulations you win");
+            gameStart(currentLevel);
+            selectedCard1 = 0;
+            selectedCard2 = 0;
+            value1 = 0;
+            value2 = 0;
+            matchedPairs = 0;
+            props.resetMoves();
+            /* props.restartLevel(); */
         }
     }
     
@@ -28,18 +95,25 @@ const Card = (props) => {
         } 
         
         else if (selectedCard1 && selectedCard2) {
-            document.getElementById(selectedCard1).style.display = "none";
-            document.getElementById(selectedCard2).style.display = "none";
-            selectedCard1 = 0;
-            value1 = 0;
-            selectedCard2 = 0;
-            value2 = 0;
-            props.incrementMoves();
+            setTimeout(() => {
+                document.getElementById(selectedCard1).style.backgroundColor = cardBackgroundColor;
+                document.getElementById(selectedCard2).style.backgroundColor = cardBackgroundColor;
+                document.getElementById(selectedCard1).style.color = cardBackgroundColor;
+                document.getElementById(selectedCard2).style.color = cardBackgroundColor;
+                document.getElementById(selectedCard1).style.pointerEvents = "auto";
+                document.getElementById(selectedCard2).style.pointerEvents = "auto";
+                selectedCard1 = 0;
+                value1 = 0;
+                selectedCard2 = 0;
+                value2 = 0;
+                props.incrementMoves();
+                }, 500);
         }
     }
-        console.log("Card is called")
         const handleCardClick = (id) => {
-            document.getElementById(id).style.display = "block";
+            document.getElementById(id).style.backgroundColor = "#ffffff";
+            document.getElementById(id).style.color = "rgb(0, 0, 0)";
+            document.getElementById(id).style.pointerEvents = "none";
             if(selectedCard1 === 0){
                 selectedCard1 = props.id;
                 value1 = props.value;
@@ -53,8 +127,18 @@ const Card = (props) => {
     
 
         return (
-            <div className="card" onClick={() => handleCardClick(props.id)}>
-                <p className="card-number" id={props.id} style={{display: "none"}}>
+            <div 
+                className="card" 
+                 onClick={() => handleCardClick(props.id)}
+                 id={props.id}
+                 style={{
+                     backgroundColor: cardBackgroundColor, 
+                     color: cardBackgroundColor,
+                     width: cardWidth,
+                     height: cardHeight,
+                    }}
+                 >
+                <p className="card-number">
                     {props.value}
                 </p>
             </div>
